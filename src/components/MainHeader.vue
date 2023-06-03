c790d07d132f2f107d652c07c93c8ac41882b763
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-03 21:08:33 +0100

    Updated styles in header

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,6 +1,8 @@
 <template>
-  <button>game 1</button>
-  <button>game 2</button>
+    <header>
+        <button>game 1</button>
+        <button>game 2</button>
+    </header>
 </template>
 
 <script>
@@ -10,5 +12,11 @@ export default {
 </script>
 
 <style>
+header {
+    display: flex;
+    gap: 30px;
+    align-items: center;
+    justify-content: center;
+}
 
 </style>
