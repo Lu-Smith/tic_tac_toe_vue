01aa2f56030a4865b60d94097f0a83af154c4e84
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-03 20:43:59 +0100

    Created MainFooter.vue

---
+++ src/components/MainFooter.vue
@@ -0,0 +1,8 @@
+<template>
+    <footer>Coded by <a href="https://www.lunasmithart.com/" target="_blank">Luna Smith</a>, open-sourced on 
+    <a href="https://github.com/Lu-Smith/tic_tac_toe_vue" target="_blank">GitHub</a>.</footer>
+  </template>
+  
+  <style>
+ 
+  </style>
