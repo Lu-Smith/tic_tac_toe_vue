04550ba3d8c93e69dd0f39a4855c522d9b4985ff
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-03 22:09:41 +0100

    Updated BasicGrid.vue

--- src/components/BasicGame/BasicGrid.vue
+++ src/components/BasicGame/BasicGrid.vue
@@ -1,17 +1,45 @@
 <template>
   <div class="grid">
-    <div class="cell">
-        cell
+    <div class="cell" v-for="cell in cells" :key="cell">
+        {{ cell }}
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      cells: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    };
+  }
 
 }
 </script>
 
 <style>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  gap: 10px;
+  justify-items: center;
+  align-items: center;
+  margin: 30px auto;
+  width: 290px;
+  height: 290px;
 
+}
+
+.cell {
+  width: 90px;
+  height: 90px;
+  background: radial-gradient(circle at 10% 20%, rgb(253, 239, 132) 0%, rgb(247, 198, 169) 54.2%, rgb(21, 186, 196) 100.3%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  border: 3px solid rgb(243, 184, 96);
+  box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.3);
+}
 </style>
