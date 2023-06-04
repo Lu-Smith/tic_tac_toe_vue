185c1827a7001b54bd22d38f402d3128563dcdea
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-04 23:41:12 +0100

    Updated logic in AdvancedGame.vue

--- src/components/AdvancedGame/AdvancedGame.vue
+++ src/components/AdvancedGame/AdvancedGame.vue
@@ -34,27 +34,31 @@ export default {
             this.move = 'X'
           }
           this.cells[index] = this.move; 
-          if (this.cells[0] === 'O' && this.cells[1] === 'O' && this.cells[2] === 'O' || 
-              this.cells[3] === 'O' && this.cells[4] === 'O' && this.cells[5] === 'O' ||
-              this.cells[6] === 'O' && this.cells[7] === 'O' && this.cells[8] === 'O' ||
-              this.cells[0] === 'O' && this.cells[3] === 'O' && this.cells[6] === 'O' || 
-              this.cells[1] === 'O' && this.cells[4] === 'O' && this.cells[7] === 'O' ||
-              this.cells[2] === 'O' && this.cells[5] === 'O' && this.cells[8] === 'O' ||
-              this.cells[0] === 'O' && this.cells[4] === 'O' && this.cells[8] === 'O' ||
-              this.cells[2] === 'O' && this.cells[4] === 'O' && this.cells[6] === 'O' 
+          if (this.cells[0] === 'O' && this.cells[1] === 'O' && this.cells[2] === 'O' && this.cells[3] === 'O'|| 
+              this.cells[4] === 'O' && this.cells[5] === 'O' && this.cells[6] === 'O' && this.cells[7] === 'O'||
+              this.cells[8] === 'O' && this.cells[9] === 'O' && this.cells[10] === 'O' && this.cells[11] === 'O'||
+              this.cells[12] === 'O' && this.cells[13] === 'O' && this.cells[14] === 'O' && this.cells[15] === 'O'|| 
+              this.cells[0] === 'O' && this.cells[4] === 'O' && this.cells[8] === 'O' && this.cells[12] === 'O'||
+              this.cells[1] === 'O' && this.cells[5] === 'O' && this.cells[9] === 'O' && this.cells[13] === 'O'||
+              this.cells[2] === 'O' && this.cells[6] === 'O' && this.cells[10] === 'O' && this.cells[14] === 'O'||
+              this.cells[3] === 'O' && this.cells[7] === 'O' && this.cells[11] === 'O' && this.cells[15] === 'O' ||
+              this.cells[0] === 'O' && this.cells[5] === 'O' && this.cells[10] === 'O' && this.cells[15] === 'O' ||
+              this.cells[3] === 'O' && this.cells[6] === 'O' && this.cells[9] === 'O' && this.cells[12] === 'O' 
           ) {
             this.gameOver = true
             this.winner = 'Player 1'
             this.move = ''
             this.player = ''
-          } else if (this.cells[0] === 'X' && this.cells[1] === 'X' && this.cells[2] === 'X' || 
-              this.cells[3] === 'X' && this.cells[4] === 'X' && this.cells[5] === 'X' ||
-              this.cells[6] === 'X' && this.cells[7] === 'X' && this.cells[8] === 'X' ||
-              this.cells[0] === 'X' && this.cells[3] === 'X' && this.cells[6] === 'X' || 
-              this.cells[1] === 'X' && this.cells[4] === 'X' && this.cells[7] === 'X' ||
-              this.cells[2] === 'X' && this.cells[5] === 'X' && this.cells[8] === 'X' ||
-              this.cells[0] === 'X' && this.cells[4] === 'X' && this.cells[8] === 'X' ||
-              this.cells[2] === 'X' && this.cells[4] === 'X' && this.cells[6] === 'X' 
+          } else if (this.cells[0] === 'X' && this.cells[1] === 'X' && this.cells[2] === 'X' && this.cells[3] === 'X'|| 
+              this.cells[4] === 'X' && this.cells[5] === 'X' && this.cells[6] === 'X' && this.cells[7] === 'X'||
+              this.cells[8] === 'X' && this.cells[9] === 'X' && this.cells[10] === 'X' && this.cells[11] === 'X'||
+              this.cells[12] === 'X' && this.cells[13] === 'X' && this.cells[14] === 'X' && this.cells[15] === 'X'|| 
+              this.cells[0] === 'X' && this.cells[4] === 'X' && this.cells[8] === 'X' && this.cells[12] === 'X'||
+              this.cells[1] === 'X' && this.cells[5] === 'X' && this.cells[9] === 'X' && this.cells[13] === 'X'||
+              this.cells[2] === 'X' && this.cells[6] === 'X' && this.cells[10] === 'X' && this.cells[14] === 'X'||
+              this.cells[3] === 'X' && this.cells[7] === 'X' && this.cells[11] === 'X' && this.cells[15] === 'X' ||
+              this.cells[0] === 'X' && this.cells[5] === 'X' && this.cells[10] === 'X' && this.cells[15] === 'X' ||
+              this.cells[3] === 'X' && this.cells[6] === 'X' && this.cells[9] === 'X' && this.cells[12] === 'X' 
           ) {
             this.gameOver = true
             this.winner = 'Player 2'
