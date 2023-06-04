1290399ffe2e2d566b4b37c801bf9805beb00dbd
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-04 22:57:53 +0100

    Updated BasicGame.vue

--- src/components/BasicGame/BasicGame.vue
+++ src/components/BasicGame/BasicGame.vue
@@ -2,6 +2,10 @@
   <h2>Basic game</h2>
   <h3>{{ player }}</h3>
   <BasicGrid :handlePlayer="handlePlayer" :move="move" :cells="cells"/>
+  <div v-if="gameOver">
+    <h4 >Game Over</h4>
+    <h4>{{ player }} won 🥳</h4>
+  </div>
 </template>
 
 <script>
@@ -15,20 +19,35 @@ export default {
     return {
       player: 'Player 1',
       move: '',
-      cells: ['','','','','','','','','']
+      cells: ['','','','','','','','',''],
+      gameOver: false
     };
   },
   methods: {
     handlePlayer(index) {
-      if (!this.cells[index]) {
-        if (this.player === 'Player 1') {
-          this.player = 'Player 2'
-          this.move = 'O'
-        } else {
+        if (!this.cells[index]) {
+          if (this.player === 'Player 1') {
+            this.player = 'Player 2'
+            this.move = 'O'
+          
+          } else {
+            this.player = 'Player 1'
+            this.move = 'X'
+          }
+        this.cells[index] = this.move; 
+        console.log(this.cells)   
+        if (this.cells[0] === 'O' && this.cells[1] === 'O' && this.cells[2] === 'O' || 
+            this.cells[3] === 'O' && this.cells[4] === 'O' && this.cells[5] === 'O' ||
+            this.cells[6] === 'O' && this.cells[7] === 'O' && this.cells[8] === 'O' ||
+            this.cells[0] === 'O' && this.cells[3] === 'O' && this.cells[6] === 'O' || 
+            this.cells[1] === 'O' && this.cells[4] === 'O' && this.cells[8] === 'O' ||
+            this.cells[2] === 'O' && this.cells[5] === 'O' && this.cells[8] === 'O' ||
+            this.cells[0] === 'O' && this.cells[4] === 'O' && this.cells[8] === 'O' ||
+            this.cells[2] === 'O' && this.cells[4] === 'O' && this.cells[6] === 'O' 
+        ) {
+          this.gameOver = true
           this.player = 'Player 1'
-          this.move = 'X'
-        }
-        this.cells[index] = this.move;
+        }   
       }
     }
   }
